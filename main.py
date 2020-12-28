22c74f5fc130ab52d36462a22f257e8a20783780
lhridder <63594063+lhridder@users.noreply.github.com>
2020-12-28 14:57:22 +0100

    added online checks added get lamp status

--- main.py
+++ main.py
@@ -2,6 +2,12 @@
 # imports
 import configparser
 import sys
+import os
+import requests
+
+ip = ""
+key = ""
+
 
 # check config
 def createconfig():
@@ -15,7 +21,7 @@ def createconfig():
         print("Config file doesn't exist yet, making one...")
         # make config
         config = configparser.ConfigParser()
-        config['HUEBRIDGE'] = {'ip' : '', 'key' : ''}
+        config['HUEBRIDGE'] = {'ip': '', 'key': ''}
         # write to config
         with open('config.ini', 'w') as configfile:
             config.write(configfile)
@@ -24,14 +30,44 @@ def createconfig():
         sys.exit(0)
     # config exists
     print("Config exists. continueing...")
+
+
+# load config
+def loadconfig():
+    global ip, key
     config = configparser.ConfigParser()
     config.read("config.ini")
-    huebridge = config['HUEBRIDGE']
-    if "ip" not in config["HUEBRIDGE"]:
-        print("huts")
+    ip = config["HUEBRIDGE"]["ip"]
+    key = config["HUEBRIDGE"]["key"]
 
 
+# ping hue
+def checkonline():
+    response = os.system("ping -n 1 " + ip)
 
+    # and then check the response...
+    if response == 0:
+        print('Hue bridge online, continueing')
+    else:
+        print('Hue bridge offline, stopping')
+        sys.exit(0)
+
+    # check key
+    r = requests.get('http://' + ip + "/api/" + key)
+    if r.text.__contains__("unauthorized"):
+        print('Hue bridge api-key not authorized, quitting')
+        sys.exit(0)
+    else:
+        print("Huts")
+
+    rjson = r.json()
+    lights = rjson["lights"]
+    for k, v in lights.items():
+        print("Lamp " + k + ":")
+        print("  Name: " + str(lights[k]["name"]))
+        print("  On: " + str(lights[k]["state"]["on"]))
+        print("  Hue: " + str(lights[k]["state"]["hue"]))
+        print("  Brightness: " + str(lights[k]["state"]["bri"]))
 
 
 # main script
@@ -39,3 +75,7 @@ if __name__ == '__main__':
     print('LHue starting')
     # create config
     createconfig()
+    # load config
+    loadconfig()
+    # continue huts
+    checkonline()
