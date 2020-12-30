43aed6702630aa865610d83688646fa92c0a8637
lhridder <63594063+lhridder@users.noreply.github.com>
2020-12-30 13:05:23 +0100

    - added brightness, toggle and setcolor functions - simplified base url and body

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import requests
 
 ip = ""
 key = ""
+base = ""
 
 
 # check config
@@ -14,9 +15,9 @@ def createconfig():
     print('Checking if config exists...')
     # check file
     try:
-        f = open("config.ini")
+        file = open("config.ini")
         # Do something with the file
-        f.close()
+        file.close()
     except IOError:
         print("Config file doesn't exist yet, making one...")
         # make config
@@ -29,28 +30,29 @@ def createconfig():
         print("Config created! stopping...")
         sys.exit(0)
     # config exists
-    print("Config exists. continueing...")
+    print("Config exists. continuing...")
 
 
 # load config
 def loadconfig():
-    global ip, key
+    global ip, key, base
     config = configparser.ConfigParser()
     config.read("config.ini")
     ip = config["HUEBRIDGE"]["ip"]
     key = config["HUEBRIDGE"]["key"]
+    base = "http://" + ip + "/api/" + key + "/"
 
 
-# ping hue
+# ping hue / Checks if Hue online
 def checkonline():
     response = os.system("ping -n 1 " + ip)
 
     # and then check the response...
     if response == 0:
-        print('Hue bridge online, continueing')
+        print('Hue bridge online, continuing')
     else:
         print('Hue bridge offline, stopping')
-        sys.exit(0)
+        sys.exit(0)  # stops if offline
 
     # check key
     r = requests.get('http://' + ip + "/api/" + key)
@@ -58,16 +60,92 @@ def checkonline():
         print('Hue bridge api-key not authorized, quitting')
         sys.exit(0)
     else:
-        print("Huts")
+        print("Authorized")
 
+
+# get lamp info
+def getinfo(lamp=None):
+    # get all lights
+    if lamp is None:
+        print("get all light info")
+        r = requests.get(base)
+        # format json
+        rjson = r.json()
+        # get lights
+        lights = rjson["lights"]
+        # get every light
+        for k, v in lights.items():
+            print("Lamp " + k + ":")
+            print("  Name: " + str(lights[k]["name"]))
+            print("  On: " + str(lights[k]["state"]["on"]))
+            print("  Hue: " + str(lights[k]["state"]["hue"]))
+            print("  Brightness: " + str(lights[k]["state"]["bri"]))
+    # get one light
+    else:
+        print("get light " + lamp + " info")
+
+
+# turn on/off
+def togglelight(lamp, boolean):
+    print("Setting lamp " + str(lamp) + " to " + boolean)
+    # form url
+    url = base + "lights/" + str(lamp) + "/state"
+    # form body
+    body = '{"on":' + boolean + '}'
+    r = requests.put(url, body)
+    # format response
     rjson = r.json()
-    lights = rjson["lights"]
-    for k, v in lights.items():
-        print("Lamp " + k + ":")
-        print("  Name: " + str(lights[k]["name"]))
-        print("  On: " + str(lights[k]["state"]["on"]))
-        print("  Hue: " + str(lights[k]["state"]["hue"]))
-        print("  Brightness: " + str(lights[k]["state"]["bri"]))
+    print(rjson)
+    # filter response
+    response = rjson[0]["success"]
+    for k, v in response.items():
+        # filter lamp no
+        lamp = k.replace("/lights/", "").replace("/state/on", "")
+        # filter lamp status
+        if v:
+            status = "on"
+        else:
+            status = "off"
+        # print result
+        print("lamp " + lamp + " is now " + status)
+
+
+# zet brightness
+def setbrightness(lamp, brightness):
+    print("Setting lamp ", str(lamp), " to brightness: ", brightness)
+    # form url
+    url = base + "lights/" + str(lamp) + "/state"
+    # form body
+    body = '{"bri":' + str(brightness) + '}'
+    r = requests.put(url, body)
+    # format response
+    rjson = r.json()
+    # print(rjson)
+    # filter response
+    response = rjson[0]["success"]
+    for k, v in response.items():
+        # filter lamp no
+        lamp = k.replace("/lights/", "").replace("/state/bri", "")
+        # print result
+        print("lamp " + lamp + " brightness is now " + str(v))
+
+
+# zet kleur
+def setcolor(lamp, color):
+    print("setting lamp 1 to " + str(color))
+    url = base + "lights/" + str(lamp) + "/state"
+    body = '{"hue":' + str(color) + '}'
+    r = requests.put(url, body)
+    # format response
+    rjson = r.json()
+    response = rjson[0]["success"]
+    print(rjson)
+    # light = rjson["lights"][1]
+    for k, v in response.items():
+        # filter lamp no
+        lamp = k.replace("/lights/", "").replace("/state/hue", "")
+        # print result
+        print("lamp " + lamp + " hue is now " + str(v))
 
 
 # main script
@@ -79,3 +157,11 @@ if __name__ == '__main__':
     loadconfig()
     # continue huts
     checkonline()
+    # get all lamp info
+    getinfo()
+    # huts lamp aan
+    togglelight(1, "true")
+    # zet brightness
+    setbrightness(1, 254)
+    # zet kleur
+    setcolor(1, 40000)
